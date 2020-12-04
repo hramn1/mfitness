86eab9c49eab8b96b1befcb1194af232b17e6f39
Stas <stmihailov@yandex.ru>
2020-12-04 03:22:30 +0300

    edit 1.61

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,14 @@
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import '/../../node_modules/swiper/swiper-bundle.css';
-
-const swiper = new Swiper();
+// // import Swiper JS
+// import Swiper from 'swiper';
+// // import Swiper styles
+// import '/../../node_modules/swiper/swiper-bundle.css';
+//
+// const swiper = new Swiper();
+const body = document.querySelector('body')
+function printDom(node, prefix){
+  console.log(prefix + node.nodeName);
+  for(let i = 0; i < node.childNodes.length; i++){
+    printDom(node.childNodes[i], prefix + '\t');
+  }
+}
+printDom(body, '');
