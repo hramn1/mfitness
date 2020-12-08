7ce492f3e1ef608e471753566a8fd923e48fad2e
Stas <stmihailov@yandex.ru>
2020-12-08 20:46:17 +0300

    edit -v2.0

--- js/swiper.js
+++ js/swiper.js
@@ -4,11 +4,4 @@
 // import '/../../node_modules/swiper/swiper-bundle.css';
 //
 // const swiper = new Swiper();
-const body = document.querySelector('body')
-function printDom(node, prefix){
-  console.log(prefix + node.nodeName);
-  for(let i = 0; i < node.childNodes.length; i++){
-    printDom(node.childNodes[i], prefix + '\t');
-  }
-}
-printDom(body, '');
+
