3a6a262b2e045910196bc5dea8d13314645c4dac
Stas <stmihailov@yandex.ru>
2020-12-14 23:08:17 +0300

    slider

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,28 @@
-var swiper = new Swiper('.swiper-container', {
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-	el: '.swiper-pagination',
-	},
+// var swiper = new Swiper('.swiper-container', {
+// 	navigation: {
+// 		nextEl: '.swiper-button-next',
+// 		prevEl: '.swiper-button-prev',
+// 	},
+// 	pagination: {
+// 	el: '.swiper-pagination',
+// 	},
+// });
+
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  direction: "vertical",
+});
+var galleryTop = new Swiper('.gallery-top', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: galleryThumbs
+  }
 });
