01211ac3bab82aaa1d602e5afdc95c01246ba91e
hramn1 <stmihailov@yandex.ru>
2021-01-27 16:08:45 +0300

    edit v 0.8

--- js/menu-head.js
+++ js/menu-head.js
@@ -1,5 +1,7 @@
 (function (){
   const btnOpenMenu = document.querySelector(`.bottom-head__catalog-menu`);
+  const btnOpenTopMenu = document.querySelector(`.top-links-mobile__link`);
+  const btnMainMenuOpen = document.querySelector(`.mobile-open-menu-btn`);
   const openedMenu = (evt)=>{
     const dropDownMenu = document.querySelector(`.bottom-head__dropdown-menu`);
     const iconMenu = document.querySelector(`.bottom-head__menu-btn`);
@@ -11,5 +13,15 @@
       iconMenu.classList.add(`bottom-head__menu-btn--closed`)
     }
   }
-  btnOpenMenu.addEventListener('click', openedMenu)
+  const openedTopMenu = (evt) =>{
+    const dropDownTopMenu = document.querySelector(`.top-links-mobile-dropdown`);
+    dropDownTopMenu.classList.toggle('top-links-mobile-dropdown--show')
+  }
+  const openedMainMenu = (evt) =>{
+    const mainMenuMobile = document.querySelector(`.bottom-head__menu`);
+    mainMenuMobile.classList.toggle('bottom-head__menu--show')
+  }
+  btnOpenTopMenu.addEventListener(`click`, openedTopMenu);
+  btnOpenMenu.addEventListener('click', openedMenu);
+  btnMainMenuOpen.addEventListener('click', openedMainMenu);
 })();
